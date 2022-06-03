153cd8304f1419e0896f10a5eeb7b25ab058557a
Ing.JEDagobethM <dagobeth.montes@pragma.com.co>
2022-06-02 19:07:48 -0500

    Se implementa la interfaz CrudRepository para Producto

--- src/main/java/com/platzi/market/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/com/platzi/market/persistence/crud/ProductoCrudRepository.java
@@ -1,2 +1,7 @@
-package com.platzi.market.persistence.crud;public interface ProductoCrudRepository {
+package com.platzi.market.persistence.crud;
+
+import com.platzi.market.persistence.entity.Producto;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProductoCrudRepository extends CrudRepository<Producto,Integer> {
 }
