652a55634cca5784313b9b8caa74622308476062
Ing.JEDagobethM <dagobeth.montes@pragma.com.co>
2022-06-07 13:54:41 -0500

    Implementando el funcionamiento de Controlador para Compras y Compras Producto.

--- src/main/java/com/platzi/market/persistence/mapper/PurchaseItemMapper.java
+++ src/main/java/com/platzi/market/persistence/mapper/PurchaseItemMapper.java
@@ -8,19 +8,21 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
 
-@Mapper(componentModel = "spring", uses = ProductMapper.class)
+@Mapper(componentModel = "spring", uses = {ProductMapper.class})
 public interface PurchaseItemMapper {
     @Mappings({
             @Mapping(source = "id.idProducto", target = "productId"),
             @Mapping(source = "cantidad", target = "quantity"),
-            @Mapping(source = "estado", target = "active"),
+            @Mapping(source = "estado", target = "active")
+
     })
-    PurchaseItem toPurchaseItem(CompraProducto compraProducto);
+    PurchaseItem toPurchaseItem(CompraProducto producto);
+
     @InheritInverseConfiguration
     @Mappings({
             @Mapping(target = "compra", ignore = true),
             @Mapping(target = "producto", ignore = true),
-            @Mapping(target = "id.idCompra", ignore = true),
+            @Mapping(target = "id.idCompra", ignore = true)
     })
-    CompraProducto toCompraProducto(Purchase purchase);
+    CompraProducto toCompraProducto(PurchaseItem item);
 }
